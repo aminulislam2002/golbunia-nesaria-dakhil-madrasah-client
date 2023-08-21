66ce29f56c07ffe5c59775719d2dff7d31e8ed21
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-08-21 04:25:36 -0700

    students routines layout design

--- src/Layouts/StudentsRoutineLayout.jsx
+++ src/Layouts/StudentsRoutineLayout.jsx
@@ -5,44 +5,55 @@ import NavBar from "../Pages/Shared/NavBar/NavBar";
 const StudentsRoutineLayout = () => {
   const options = (
     <>
-      <li className="lg:mb-2">
-        <button className="lg:text-xl font-semibold text-gray-700">Filter menu</button>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
-      <li className="mb-2">
-        <h1 className="text-xl font-semibold text-gray-700">Filter menu</h1>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
       </li>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
+      </li>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
+      </li>
+      <li className="mb-2 px-5 py-1 rounded text-white hover:text-black hover:bg-white mx-2">
+        <button className="lg:text-xl font-semibold">Filter menu</button>
+      </li>
+      {/* Add more menu items here */}
     </>
   );
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <div className="hidden lg:block">
@@ -51,7 +62,7 @@ const StudentsRoutineLayout = () => {
       <div className="drawer container mx-auto">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content lg:flex gap-1">
-          <div className="w-full lg:w-3/12 bg-base-300 lg:mt-16 lg:overflow-y-auto lg:max-h-[100vh]">
+          <div className="w-full lg:w-3/12 bg-blue-800 lg:mt-16 overflow-y-auto max-h-[100vh]">
             <div className="flex justify-start">
               <div className="flex-none lg:hidden">
                 <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
@@ -59,12 +70,12 @@ const StudentsRoutineLayout = () => {
                 </label>
               </div>
               <div className="px-2 w-auto lg:w-full">
-                <div className="ms-2 text-lg font-semibold text-gray-700">Filter as your choice</div>
+                <div className="ms-2 text-lg font-semibold">Filter as your choice</div>
               </div>
             </div>
             <div className="flex-none hidden lg:block mt-4">
-              <ul className="menu menu-vertical">
-             {/* Add more menu items here */}
+              <ul className="menu-vertical">
+                {/* Add more menu items here */}
                 {options}
               </ul>
             </div>
@@ -77,7 +88,7 @@ const StudentsRoutineLayout = () => {
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" className="drawer-overlay overflow-x-auto max-h-[100vh]"></label>
-          <ul className="menu p-4 max-w-[50vh] bg-base-200 overflow-y-auto">
+          <ul className="p-4 w-52 bg-blue-800 h-full overflow-y-auto">
             {/* Add more sidebar items here */}
             {options}
           </ul>
